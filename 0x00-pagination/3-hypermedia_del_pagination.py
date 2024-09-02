af83f12ff0cceef23f9525ed0e9c30f3cfdfd122
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-09-02 22:58:31 +0200

    [Update] - Hypermedia Del Pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,8 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """get_hyper_index method
+        """
         data = self.indexed_dataset()
         assert index is not None and index >= 0 and index <= max(data.keys())
 

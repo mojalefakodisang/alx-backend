293ac7b6ba94fe170abe9e59e585cf3758075b16
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-09-04 23:52:45 +0200

    [Update] - LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -14,25 +14,21 @@ class LIFOCache(BaseCaching):
         """ Initialize
         """
         super().__init__()
+        self.order = []
 
     def put(self, key, item):
         """ Puts cache in the cache dictionary
         """
         if key and item:
-            if key in self.cache_data:
-                del self.cache_data[key]
-
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                last_key = next(reversed(self.cache_data))
-                print("DISCARD:", last_key)
-                del self.cache_data[last_key]
-
+            if key not in self.cache_data:
+                if len(self.cache_data) >= self.MAX_ITEMS:
+                    last = self.order.pop()
+                    print(f"DISCARD: {last}")
+                    del self.cache_data[last]
             self.cache_data[key] = item
+            self.order.append(key)
 
     def get(self, key):
         """ Gets the value of specific key
         """
-        if not key or key not in self.cache_data.keys():
-            return None
-
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)

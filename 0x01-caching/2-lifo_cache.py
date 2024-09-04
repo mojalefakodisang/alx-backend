e610f448411d651f8133f6789e362a9402bf8dd7
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-09-04 23:49:47 +0200

    [Update] - LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -24,7 +24,7 @@ class LIFOCache(BaseCaching):
 
             if len(self.cache_data) >= self.MAX_ITEMS:
                 last_key = next(reversed(self.cache_data))
-                print("Discard: ", last_key)
+                print("DISCARD:", last_key)
                 del self.cache_data[last_key]
 
             self.cache_data[key] = item
